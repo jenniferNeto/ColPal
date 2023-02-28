d1841588bc9d0d55e3f901b40e1043841f192307
Shane Arcaro <smarcaro@gmail.com>
2023-02-28 12:38:21 -0500

    Adding more attributes to the pipeline model

--- backend/pipeline/models.py
+++ backend/pipeline/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from datetime import datetime, timedelta
+from django.contrib.auth import get_user_model
 
 class Pipeline(models.Model):
     title = models.CharField(max_length=40)
+    # created_by = models.ForeignKey(get_user_model)
     creation_date = models.DateTimeField(default=datetime.now)
     upload_frequency = models.DurationField(default=timedelta)
+    is_approved = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    approved_date = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         pass
--- backend/pipeline/serializers.py
+++ backend/pipeline/serializers.py
@@ -3,10 +3,20 @@ from .models import Pipeline
 
 
 class PipelineSerializer(serializers.ModelSerializer):
+    # approved_date = serializers.SerializerMethodField()
+
     class Meta:
         model = Pipeline
         fields = [
             'title',
             'creation_date',
-            'upload_frequency'
+            'upload_frequency',
+            'is_approved',
+            'is_active',
+            'approved_date'
         ]
+
+    # def get_approved_date(self, obj):
+    #     if obj is None:
+    #         return None
+    #     return obj.data
