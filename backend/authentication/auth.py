b035449def40fd7e826ca523c9bbe5ff6dfff856
Shane Arcaro <smarcaro@gmail.com>
2023-02-28 19:55:42 -0500

    Passwordless authentication now works for non super-users

--- backend/authentication/auth.py
+++ backend/authentication/auth.py
@@ -1,18 +1,27 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.contrib.auth.backends import ModelBackend
 
 
 class PasswordlessAuthentication(ModelBackend):
     """
-    Authenticate users without checking their password. This module should be deleted during production and the
-    backend/settings.py should have the AUTHENTICATION_BACKENDS flag deleted. ***CURRENTLY DISABLED TO LOG INTO THE ADMIN***
+    Authenticate regular users without checking their password
+    Admin users are still authenticated using the username and password combination
+    ** This module should be replaced with a real custom backend authentication module **
     """
-    def authenticate(self, request, username=None):
+    def authenticate(self, request, username=None, password=""):
         try:
             user = User.objects.get(username=username)
-            return user
+            if user.is_superuser and user.check_password(password):
+                return user
+            # Regular users 'need' a password but it won't be used to authenticate
+            elif not user.is_superuser:
+                return user
+            else:
+                return None
         except User.DoesNotExist:
-            return None
+            print("User does not exist")
+            raise ValidationError("Invalid credentials")
 
     def get_user(self, user_id):
         try:
