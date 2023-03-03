064bfd3e0da9ea00980fcb6a5c7dd0c7f50bd63c
Shane Arcaro <smarcaro@gmail.com>
2023-03-03 13:31:35 -0500

    Created tests for every Pipeline endpoint available currently

--- backend/pipeline/test_endpoints.py
+++ backend/pipeline/test_endpoints.py
@@ -1,83 +1,74 @@
-from django.test import TestCase, Client
-from datetime import timedelta
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 from .models import Pipeline
 
 
-class PipelineEndpointTestCase(TestCase):
+class PipelineEndpointTestCase(APITestCase):
     """
-    Test cases for the creation and modification of Pipeline objects
-    .all() warnings can be ignored
+    Test cases for Pipeline endpoints
     """
     def setUp(self):
         Pipeline.objects.all().delete()
         Pipeline.objects.create(title='Test Pipeline')
 
-
     def test_pipelines_listview(self):
         """Test the pipelines listview endpoint"""
-        client = Client()
-        response = client.get('/pipelines/', follow=True)
-        self.assertEquals(response.status_code, 200)
+        response = self.client.get("/pipelines/", follow=True)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
 
     def test_pipelines_detailview(self):
-        """Test the individial pipelines view"""
-        client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        response = client.get('/pipelines/1', follow=True)
-        self.assertEquals(response.status_code, 200)
+        """Test the pipelines individal detail endpoint"""
+        response = self.client.get("/pipelines/1/", follow=True)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_pipelines_detailview_invalid(self):
+        """Test the pipelines individal detail endpoint for failure"""
+        response = self.client.get("/pipelines/2/", follow=True)
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_pipelines_updateview_get(self):
+        """Test the pipelines get update endpoint"""
+        data = {'title': "Title", 'upload_frequency': "00:00:10", 'update_reason': "Updated", 'is_active': True}
+        response = self.client.get("/pipelines/1/update/", data=data, follow=True)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_pipelines_updateview_get_invalid(self):
+        """Test non-existant pipeline update endpoint"""
+        data = {'title': "Title", 'upload_frequency': "00:00:10", 'update_reason': "Updated", 'is_active': True}
+        response = self.client.get("/pipelines/2/update/", data=data, follow=True)
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_pipelines_updateview_put(self):
+        """Test the pipelines put update endpoint"""
+        data = {'title': "Title", 'upload_frequency': "00:00:10", 'update_reason': "Updated", 'is_active': True}
+        response = self.client.put("/pipelines/1/update/", data=data, follow=True)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_pipelines_updateview_put_invalid_upload_frequency(self):
+        """Test the pipelines put update endpoint invalid upload frequency"""
+        data = {'title': "Title", 'upload_frequency': "e", 'update_reason': "Updated", 'is_active': True}
+        response = self.client.put("/pipelines/1/update/", data=data, follow=True)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_pipelines_updateview_put_invalid_update_reason(self):
+        """Test the pipelines put update endpoint invalid update_reason"""
+        data = {'title': "Title", 'upload_frequency': "0", 'is_active': True}
+        response = self.client.put("/pipelines/1/update/", data=data, follow=True)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_pipelines_history(self):
-        """Test the individial pipelines history"""
-        client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        response = client.get('/pipelines/1/history', follow=True)
-        self.assertEquals(response.status_code, 200)
+        """Test the pipelines get history"""
+        response = self.client.get("/pipelines/1/history/", follow=True)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
 
-    def test_pipelines_update(self):
-        """Test the individial pipelines history"""
-        client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        response = client.patch('/pipelines/1/update', {'title': "New Title"}, follow=True)
-        self.assertEquals(response.status_code, 200)
+    def test_pipelines_history_invalid(self):
+        """Test the pipelines get history"""
+        response = self.client.get("/pipelines/2/history/", follow=True)
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    # Do same 405 error trick that worked with update serializers
-    # def test_pipelines_create(self):
-    #     """Test the individial pipelines history"""
-    #     client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-    #     response = client.patch('/pipelines/1/update', {'title': "New Title"}, follow=True)
-    #     self.assertEquals(response.status_code, 200)
-
-    def test_pipelines_invalid_detailview(self):
-        """Test the individial pipelines view"""
-        client = Client()
-        response = client.get('/pipelines/2', follow=True)
-        self.assertEquals(response.status_code, 404)
-
-    def test_pipeline_save_upload_frequency(self):
-        """Test updating pipeline upload_frequency"""
-        pipeline = Pipeline.objects.all()[0]
-        pipeline.upload_frequency = timedelta(days=1)
-        pipeline.save()
-
-        self.assertEquals(pipeline.upload_frequency, timedelta(days=1))
-        self.assertNotEquals(pipeline.last_modified, None)
-        self.assertEquals(pipeline.history.all().count(), 2)
-
-    def test_pipeline_save_is_approved(self):
-        """Test updating pipeline is_approved"""
-        pipeline = Pipeline.objects.all()[0]
-        pipeline.is_approved = True
-        pipeline.save()
-
-        self.assertEquals(pipeline.is_approved, True)
-        self.assertNotEquals(pipeline.last_modified, None)
-        self.assertNotEquals(pipeline.approved_date, None)
-        self.assertEquals(pipeline.history.all().count(), 2)
-
-    def test_pipeline_save_is_active(self):
-        """Test updating pipeline is_active"""
-        pipeline = Pipeline.objects.all()[0]
-        pipeline.is_active = False
-        pipeline.save()
-
-        self.assertEquals(pipeline.is_active, False)
-        self.assertNotEquals(pipeline.last_modified, None)
-        self.assertEquals(pipeline.history.all().count(), 2)
+    def test_pipelines_create(self):
+        """Test the creation of a new pipeline"""
+        data = {'title': "Title", 'upload_frequency': "0", 'is_active': False}
+        response = self.client.post("/pipelines/create/", data=data, follow=True)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
