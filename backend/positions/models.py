891f0e977f7784abcc48673a2ee8f5b8cb967863
Shane Arcaro <smarcaro@gmail.com>
2023-03-04 23:22:40 -0500

    Cleaned up pipeline positions code to use inheritance for models and serializers

--- backend/positions/models.py
+++ backend/positions/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 from django.contrib.auth import get_user_model
 
 from pipeline.models import Pipeline
@@ -10,28 +9,16 @@ class PipelineUser(models.Model):
     pipeline_id = pipeline.primary_key
 
     class Meta:
+        abstract = True
+
+class Viewer(PipelineUser):
+    class Meta:
         unique_together = ('user', 'pipeline')
 
-class Viewer(models.Model):
-    viewer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
-    pipeline = models.ForeignKey(Pipeline, on_delete=models.CASCADE, null=True)
-    pipeline_id = pipeline.primary_key
-
+class Uploader(PipelineUser):
     class Meta:
-        unique_together = ('viewer', 'pipeline')
+        unique_together = ('user', 'pipeline')
 
-class Uploader(models.Model):
-    uploader = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
-    pipeline = models.ForeignKey(Pipeline, on_delete=models.CASCADE, null=True)
-    pipeline_id = pipeline.primary_key
-
+class Manager(PipelineUser):
     class Meta:
-        unique_together = ('uploader', 'pipeline')
-
-class Manager(models.Model):
-    manager = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
-    pipeline = models.ForeignKey(Pipeline, on_delete=models.CASCADE, null=True)
-    pipeline_id = pipeline.primary_key
-
-    class Meta:
-        unique_together = ('manager', 'pipeline')
+        unique_together = ('user', 'pipeline')
--- backend/positions/seralizers.py
+++ backend/positions/seralizers.py
@@ -2,48 +2,30 @@ from rest_framework import serializers
 
 from .models import Viewer, Uploader, Manager
 
-# I don't like how this code is repeated but when trying to abstract away
-# the fields into a base class the serializer would complain that the fields
-# attribute isn't set but setting it would mean the values need to be redeclared
-
-class ViewerSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True, source="viewer.pk")
-    username = serializers.CharField(read_only=True, source='viewer.username')
-    email = serializers.CharField(read_only=True, source='viewer.email')
+class PipelineUserSerializer(serializers.ModelSerializer):
+    """
+    Base position serializer that serializers the user foreign key
+    Subclasses of this class need to set a model in the Meta innerclass
+    """
+    id = serializers.IntegerField(read_only=True, source="user.pk")
+    username = serializers.CharField(read_only=True, source='user.username')
+    email = serializers.CharField(read_only=True, source='user.email')
 
     class Meta:
+        fields = [
+            'id',
+            'username',
+            'email'
+        ]
+
+class ViewerSerializer(PipelineUserSerializer):
+    class Meta(PipelineUserSerializer.Meta):
         model = Viewer
 
-        fields = [
-            'id',
-            'username',
-            'email'
-        ]
-
-class UploaderSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True, source="uploader.pk")
-    username = serializers.CharField(read_only=True, source='uploader.username')
-    email = serializers.CharField(read_only=True, source='uploader.email')
-
-    class Meta:
+class UploaderSerializer(PipelineUserSerializer):
+    class Meta(PipelineUserSerializer.Meta):
         model = Uploader
 
-        fields = [
-            'id',
-            'username',
-            'email'
-        ]
-
-class ManagerSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True, source="manager.pk")
-    username = serializers.CharField(read_only=True, source='manager.username')
-    email = serializers.CharField(read_only=True, source='manager.email')
-
-    class Meta:
+class ManagerSerializer(PipelineUserSerializer):
+    class Meta(PipelineUserSerializer.Meta):
         model = Manager
-
-        fields = [
-            'id',
-            'username',
-            'email'
-        ]
