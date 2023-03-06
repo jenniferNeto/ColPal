33fb195297a289bf743d904b6c393a7934792028
Shane Arcaro <smarcaro@gmail.com>
2023-03-06 01:23:13 -0500

    Fixed bug with user authentication checking

--- backend/pipeline/utils.py
+++ backend/pipeline/utils.py
@@ -1,14 +1,18 @@
 from django.core.exceptions import PermissionDenied
 
 
-def check_user_permissions(request, pk, model):
+def is_user_allowed(request, pk, model):
     # Get current user instance
     user = request.user
 
     # Get managers for the current pipeline
     group = model.objects.filter(pipeline_id=pk).values_list('user', flat=True)
-    print('Group:', group)
 
     # Staff users can perform any operation
-    if not user.is_staff and user.id not in group:
+    if not (user.is_staff or user.id in group):
+        return False
+    return True
+
+def check_user_permissions(request, pk, model):
+    if not is_user_allowed(request, pk, model):
         raise PermissionDenied
