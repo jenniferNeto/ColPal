f44bd57a806a226aabe01c329e85ad8252cad828
Shane Arcaro <smarcaro@gmail.com>
2023-03-13 15:33:17 -0400

    Fixing how users are authenticated to properly compare user id values

--- backend/authentication/utils.py
+++ backend/authentication/utils.py
@@ -1,44 +1,21 @@
 from django.core.exceptions import PermissionDenied
+from django.db.models.query import QuerySet
 from django.contrib.auth.models import User
 
 from positions.models import PipelineUser
-from pipeline.models import Pipeline
 
 def is_user_allowed(request, pk, model):
     # Get current user instance
     user: User = request.user
 
-    print("Request:", request)
-    print("Primary Key:", pk)
-    print("Model:", model)
-    print("User:", user)
+    # Get positions for the current pipeline
+    positions: QuerySet[PipelineUser] = model.objects.filter(pipeline_id=pk)
 
-    # Get managers for the current pipeline
-    position: PipelineUser = model.objects.filter(pipeline_id=pk)
-    position2 = model.objects.filter(pipeline_id=pk)
-    p = model.objects.all()
+    # Get user object for each found position
+    user_positions = [obj.user.pk if obj.user is not None else None for obj in positions]
 
-    print("Position:", position)
-    print("Position 2:", position2)
-    print("Unfiltered:", p)
-
-    print()
-    print("All pipelines:")
-    [print("Pipeline ID:", e.pk) for e in Pipeline.objects.all()]
-    print("======================================================")
-
-    a = p[0] if p.count() > 0 else None
-    if a is not None:
-        print("Pipeline ID:", a.pipeline_id)
-    print()
-
-    # Staff users can perform any operation
-    # print("======================================================")
-    # print(user.pk)
-    # print(position)
-    # print(user.pk in position)
-    # print("======================================================")
-    if not (user.is_superuser or user.pk in position):
+    # If user is not a super user and is not found in user_positions return false
+    if not (user.is_superuser or user.pk in user_positions):
         return False
     return True
 
