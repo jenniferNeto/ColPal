f4fa384dec7852cb07a3381f41912777024509cb
Shane Arcaro <smarcaro@gmail.com>
2023-03-14 12:34:40 -0400

    Should fix the migration auth_tables error

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,9 +5,9 @@ from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('authentication.urls')),
     path('pipelines/', include('pipeline.urls')),
     path('pipelines/', include('positions.urls')),
     path('pipelines/', include('request.urls')),
-    path('users/', include('authentication.urls')),
     path('', home_view),
 ]
