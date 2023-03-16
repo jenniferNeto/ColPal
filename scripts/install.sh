ccf7dbbfa982ad8aece1a262b7c0b3394e7d5665
Shane Arcaro <smarcaro@gmail.com>
2023-03-15 22:47:39 -0400

    Don't need scripts for python virtual environment because of docker containers

--- scripts/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install the required packages
-pip3.10 install -r ../backend/requirements.txt && echo "Successfully installed python libraries" || echo "Failed to install required python libraries"
