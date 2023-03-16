a7dcaa7efeac80c79d2b33bdd4ffa82fc50545cf
Shane Arcaro <smarcaro@gmail.com>
2023-03-16 00:24:09 -0400

    Removing initial backend view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('authentication.urls')),
     path('pipelines/', include('pipeline.urls')),
-    path('pipelines/', include('positions.urls')),  # causing auth table error
+    path('pipelines/', include('positions.urls')),
     path('pipelines/', include('request.urls')),
-    path('', home_view),
 ]
