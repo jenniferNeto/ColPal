5b6f9461f2145904f6b43609aa8cbca3494aa320
Shane Arcaro <smarcaro@gmail.com>
2023-03-18 21:24:28 -0400

    Updating jwt access time

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from django.contrib.auth import login, logout, get_user_model
 from django.http import Http404
@@ -72,3 +73,6 @@ class UserLogoutAPIView(APIView):
         logout(request)
 
         return Response(status=status.HTTP_200_OK)
+
+class TokenRefresh(TokenRefreshView):
+    permission_classes = [AllowAny]
