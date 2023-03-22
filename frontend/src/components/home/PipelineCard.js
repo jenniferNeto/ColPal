5f16c109fa672d96cd9b499d40fcc46282b332ca
gianncarlo <gc348@njit.edu>
2023-03-22 16:21:42 -0400

    Designed upload button

--- frontend/src/components/home/PipelineCard.js
+++ frontend/src/components/home/PipelineCard.js
@@ -9,12 +9,12 @@ export default function PipelineCard({id, title, is_active, upload_frequency}) {
       <div className='card' style={{border: 'none', background: '#E9E7FD', cursor: "pointer"}} 
       onClick={() => navigate("/pipeline/"+id)}>
           <div className='card-body'>
-          <div class="card-title align-center d-flex justify-content-between">
+          <div className="card-title align-center d-flex justify-content-between">
             <h5>{title}</h5> <span className={`dot ${is_active ? 'active': 'inactive'}`}></span>
           </div>
           </div>
-          <div className='card-footer'>
-            {upload_frequency}
+          <div className='card-footer d-flex justify-content-between'>
+            <span>Next Upload:</span> <span>{upload_frequency}</span>
           </div>
       </div>
   
--- frontend/src/components/pipelines/PipelineUpload.js
+++ frontend/src/components/pipelines/PipelineUpload.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { faFileUpload } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function PipelineUpload() {
+
+  const handleUpload = () => {
+    console.log(e)
+  }
+
   return (
     <div class="card shadow-sm bg-white h-100">
-      <label class="form-label" for="customFile">Default file input example</label>
-      <input type="file" class="form-control" id="customFile" />
+      <div className='card-body'>
+        <h4>Upload File</h4>
+      </div>
+      <div className='card-footer'>
+      <div class="d-grid gap-2">
+        <label className="m-0 mr-2 upload-btn" style={{border: '3px dashed #605CA8'}}>
+          <FontAwesomeIcon icon={faFileUpload} /> Upload
+          <input
+            type="file"
+            onChange={handleUpload}
+            style={{ opacity: 0, position: "absolute", left: "-9999px" }}
+          />
+        </label>
+        </div>
+      </div>
     </div>
   )
 }
