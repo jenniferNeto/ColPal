cc766cbdbc8ae7af92b71b70d403011f41e25910
gianncarlo <gc348@njit.edu>
2023-03-23 00:07:31 -0400

    merged

--- backend/positions/serializers.py
+++ backend/positions/serializers.py
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-from rest_framework import serializers
-
-from django.contrib.auth import get_user_model
-
-from .models import Viewer, Uploader, Manager
-
-User = get_user_model()
-
-
-class PipelineUserSerializer(serializers.ModelSerializer):
-    """
-    Base position serializer that serializers the user foreign key
-    Subclasses of this class need to set a model in the Meta innerclass
-    """
-    id = serializers.IntegerField(read_only=True, source="user.pk")
-    username = serializers.CharField(read_only=True, source='user.username')
-    email = serializers.CharField(read_only=True, source='user.email')
-
-    class Meta:
-        fields = [
-            'id',
-            'username',
-            'email'
-        ]
-
-class ViewerSerializer(PipelineUserSerializer):
-    class Meta(PipelineUserSerializer.Meta):
-        model = Viewer
-
-class UploaderSerializer(PipelineUserSerializer):
-    class Meta(PipelineUserSerializer.Meta):
-        model = Uploader
-
-class ManagerSerializer(PipelineUserSerializer):
-    class Meta(PipelineUserSerializer.Meta):
-        model = Manager
-
-class PositionCreateSerializer(serializers.ModelSerializer):
-    # Show the userid instead of the user itself for post purposes
-    id = serializers.IntegerField(read_only=True, source="user.pk")
-
-    class Meta:
-        fields = ['id']
-
-    def get_pipeline(self, obj):
-        return obj.pipeline
-
-class ViewerCreateSerializer(PositionCreateSerializer):
-    class Meta(PositionCreateSerializer.Meta):
-        model = Viewer
-
-class UploaderCreateSerializer(PositionCreateSerializer):
-    class Meta(PositionCreateSerializer.Meta):
-        model = Uploader
-
-class ManagerCreateSerializer(PositionCreateSerializer):
-    class Meta(PositionCreateSerializer.Meta):
-        model = Manager
-=======
 from rest_framework import serializers
 
 from django.contrib.auth import get_user_model
@@ -115,4 +55,3 @@ class UploaderCreateSerializer(PositionCreateSerializer):
 class ManagerCreateSerializer(PositionCreateSerializer):
     class Meta(PositionCreateSerializer.Meta):
         model = Manager
->>>>>>> 67abf97cb6b43bec49e23020ffb2efb7720b06a1
--- backend/request/utils.py
+++ backend/request/utils.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-from django.utils.dateparse import parse_duration
-
-from .models import Request
-
-
-def createRequest(data, instance):
-    # Create a new pipeline modification request
-    mod = Request.objects.create(title="Blank", pipeline=instance)
-
-    mod.title = data['title']
-    mod.upload_frequency = parse_duration(data['upload_frequency'])
-    mod.is_active = True if 'is_acitve' in data else False
-    mod.update_reason = data['update_reason']
-    mod.save()
-=======
 from django.utils.dateparse import parse_duration
 
 from .models import Request
@@ -28,4 +12,3 @@ def createRequest(data, instance):
     mod.is_active = 'is_active' in data
     mod.update_reason = data['update_reason']
     mod.save()
->>>>>>> 67abf97cb6b43bec49e23020ffb2efb7720b06a1
