2698fd48935f52565868d5c4b68136eb0afbe481
gianncarlo <gc348@njit.edu>
2023-04-01 15:37:20 -0400

    implemented UI for upload history

--- frontend/src/components/file-upload/UploadCheckout.js
+++ frontend/src/components/file-upload/UploadCheckout.js
@@ -1,9 +1,8 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import useParser from '../../hooks/useParser';
+import CSVDisplay from '../commons/CSVDisplay';
 export default function UploadCheckout({ show, file, checkout, close }) {
-
-  const { columns, rows } = useParser(file)
 
   if (!file) return null
 
@@ -14,30 +13,8 @@ export default function UploadCheckout({ show, file, checkout, close }) {
       </Modal.Header>
       <Modal.Body>
 
-        <div className="container-fluid px-0">
-          <div className="row g-0 h-100">
-            <div className="col-lg-12 border vh-50">
-              <table className="table" >
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    {columns && columns.map((col) => <th scope="col">{col}</th>)}
-                  </tr>
-                </thead>
-                <tbody>
-                  {rows && rows.map((row, index) => (
-                    <tr>
-                      <th scope="row">{index}</th>
-                      {columns.map(col=> <td scope="col">{row[col] ?? '___'}</td>)}
-                    </tr>
-                  ))}
-
-                </tbody>
-              </table>
-            </div>
-
-          </div>
-        </div>
+      <CSVDisplay csv={file} />
+        
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={close}>
--- frontend/src/components/home/PipelineCard.js
+++ frontend/src/components/home/PipelineCard.js
@@ -1,20 +1,30 @@
-import React from 'react'
+
+import {parseFrequency} from "../../utils/functions";
 import { useNavigate } from "react-router-dom";
 
-export default function PipelineCard({id, title, is_active, upload_frequency}) {
+export default function PipelineCard({data}) {
+  const {id, title, is_active, approved, upload_frequency} = data;
   const navigate = useNavigate();
-
+  const [day, hours, minutes, seconds] = parseFrequency(upload_frequency);
+  
   return (
    
       <div className='card' style={{border: 'none', background: '#E9E7FD', cursor: "pointer"}} 
-      onClick={() => navigate("/pipeline/"+id)}>
+      onClick={() => navigate("/pipeline/"+id, {state: {data}})}>
           <div className='card-body'>
-          <div className="card-title align-center d-flex justify-content-between">
-            <h5>{title}</h5> <span className={`dot ${is_active ? 'active': 'inactive'}`}></span>
+          <div className="card-title d-flex justify-content-between">
+            <h5>{title}</h5> 
+            <div>
+            <span className={`badge me-1 rounded-pill bg-${is_active ? 'success': 'danger'}`}>
+              {is_active ? 'active': 'inactive'}</span>
+            <span className={`badge rounded-pill bg-${approved ? 'success': 'danger'}`}>
+              {approved ? 'approved': 'not approved'}</span>
+            </div>
+              
           </div>
           </div>
           <div className='card-footer d-flex justify-content-between'>
-            <span>Next Upload:</span> <span>{upload_frequency}</span>
+            <span>Upload Frequency:</span> <span>{`${day}d ${hours}h ${minutes}m  ${seconds}s `}</span>
           </div>
       </div>
   
