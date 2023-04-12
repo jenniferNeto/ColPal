cbeafa74ae463d9a672020f78c8ec6ccfdff9820
Shane Arcaro <smarcaro@gmail.com>
2023-04-12 15:21:49 -0400

    APScheduler doesn't run in github actions

--- backend/pipeline/urls.py
+++ backend/pipeline/urls.py
@@ -5,10 +5,16 @@ from .utils import cron_is_stable
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
+import os
+
 # Code is being run here since urls.py gets loaded a single time by the application
-scheduler = BackgroundScheduler()
-scheduler.start()
-scheduler.add_job(cron_is_stable, 'interval', seconds=5)
+# Need to ignore scheduler to run django tests during github workflow
+
+# To run django test cases this has to be disabled. Pipelines will not be automatically checked for stability
+if os.environ.get('IGNORE_SCHEDULER') is None or not os.environ.get('IGNORE_SCHEDULER'):
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+    scheduler.add_job(cron_is_stable, 'interval', seconds=5)
 
 # Include / at the end of endpoints or unexpected errors will be raised
 urlpatterns = [
