44ceeff136ba75008eecf71ea9100e296b2c5b0a
Samikshya_poudel <poudel4samikshya@gmail.com>
2023-04-13 13:15:15 -0400

    Update frontend-docker-entrypoint.sh

--- frontend/docker/frontend-docker-entrypoint.sh
+++ frontend/docker/frontend-docker-entrypoint.sh
@@ -3,10 +3,10 @@
 # If in production start the server
 if [ $ENVIRONMENT == "production" ]; then
     # Build the deployment version of the frontend
-    npm run build
+    #npm run build
 
     # Serve the frontend
-    npm install -g serve
+    #npm install -g serve
     serve -s build
 else
     # Return to original container to avoid exit code 0
